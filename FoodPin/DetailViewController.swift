257e6e6e8edee1480fc73138f99ce3c3171b6ad5
Jason Cheng <yannru.cheng@gmail.com>
2015-07-04 19:49:36 +0800

    Add map annotation

--- FoodPin/DetailViewController.swift
+++ FoodPin/DetailViewController.swift
@@ -85,14 +85,17 @@ class DetailViewController: UIViewController, UITableViewDataSource, UITableView
         return cell
     }
 
-    /*
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        if segue.identifier == "showMap" {
+            let destinationController = segue.destinationViewController as! MapViewController
+            destinationController.restaurant = self.restaurant
+        }
     }
-    */
 
 }
--- FoodPin/MapViewController.swift
+++ FoodPin/MapViewController.swift
@@ -9,20 +9,64 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     var restaurant:Restaurant!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.mapView.delegate = self
 
         // Do any additional setup after loading the view.
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(restaurant.location, completionHandler: {placemarks,
+        error in
+            //process placemark
+            if error != nil {
+                println(error)
+                return
+            }
+            
+            if placemarks != nil && placemarks.count > 0 {
+                let placemark = placemarks[0] as! CLPlacemark
+                
+                // Add Annotation
+                let annotation = MKPointAnnotation()
+                annotation.title = self.restaurant.name
+                annotation.subtitle = self.restaurant.type
+                annotation.coordinate = placemark.location.coordinate
+                self.mapView.showAnnotations([annotation], animated: true)
+                self.mapView.selectAnnotation(annotation, animated: true)
+            }
+        })
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        let identifier = "MyPin"
+    
+        if annotation.isKindOfClass(MKUserLocation) {
+            return nil
+        }
+    
+        // Reuse the annotation if possible
+        var annotationView = mapView.dequeueReusableAnnotationViewWithIdentifier(identifier)
+        if annotationView == nil {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView.canShowCallout = true
+        }
+
+        let leftIconView = UIImageView(frame: CGRectMake(0, 0, 53, 53))
+        leftIconView.image = UIImage(named: restaurant.image)
+        annotationView.leftCalloutAccessoryView = leftIconView
+    
+        return annotationView
     }
     
 
